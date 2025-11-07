746b53f842aeb0c1f485cc05c72afb3356e2a28d
Angvannor <3102853063@qq.com>
2025-11-07 13:21:31 +0800

    小小修改了一下页面，就这样吧

--- vocab_Notebook/src/components/word_board.vue
+++ vocab_Notebook/src/components/word_board.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-1/2 h-2/3 bg-gray-50 rounded-3xl m-auto shadow-md">
     <h1 class="text-center text-2xl font-bold m-3">Word Book</h1>
-    <el-table :data="vocabs" border class="w-9/10! m-auto">
+    <el-table :data="vocabs" border class="w-9/10! h-9/10! m-auto">
       <el-table-column label="WordName" prop="name" width="120"></el-table-column>
       <el-table-column label="Nature" prop="nature" width="80"></el-table-column>
       <el-table-column label="Trans" prop="interpretation" width="100"></el-table-column>
